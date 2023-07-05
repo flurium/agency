a15ff7cd4d3e151ccb69862ac01ef4a870454122
Roman Koshchei <romankoshchei@gmail.com>
2023-07-05 20:56:15 +0300

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,27 @@ import { defineConfig } from "astro/config"
 import svelte from "@astrojs/svelte"
 import tailwind from "@astrojs/tailwind"
 import vercel from "@astrojs/vercel/serverless"
-
 import image from "@astrojs/image"
+
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), image()],
-  output: "hybrid",
+  integrations: [
+    svelte(),
+    tailwind(),
+    image(),
+    sitemap({
+      i18n: {
+        defaultLocale: "uk",
+        locales: {
+          en: "en",
+          uk: "uk",
+        },
+      },
+    }),
+  ],
   adapter: vercel(),
+  output: "hybrid",
+  site: "https://flurium.com",
 })
