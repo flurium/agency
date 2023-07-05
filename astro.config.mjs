a20f0113d29b53975762d142f54f40175cd0e4a5
Roman Koshchei <romankoshchei@gmail.com>
2023-07-05 21:03:28 +0300

    prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,24 @@
-import { defineConfig } from "astro/config"
-import svelte from "@astrojs/svelte"
-import tailwind from "@astrojs/tailwind"
-import vercel from "@astrojs/vercel/serverless"
-import image from "@astrojs/image"
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 
-import sitemap from "@astrojs/sitemap"
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    svelte(),
-    tailwind(),
-    image(),
-    sitemap({
-      i18n: {
-        defaultLocale: "uk",
-        locales: {
-          en: "en",
-          uk: "uk",
-        },
-      },
-    }),
-  ],
+  integrations: [svelte(), tailwind(), image(), sitemap({
+    i18n: {
+      defaultLocale: "uk",
+      locales: {
+        en: "en",
+        uk: "uk"
+      }
+    }
+  }), prefetch()],
   adapter: vercel(),
   output: "hybrid",
-  site: "https://flurium.com",
-})
+  site: "https://flurium.com"
+});
--- src/components/nav/Link.svelte
+++ src/components/nav/Link.svelte
@@ -4,10 +4,22 @@
 
 </script>
 
+{#if external}
+
 <a
   class="underline-offset-4 decoration-2 decoration-zinc-300 hover:decoration-primary-400 underline font-semibold cursor-pointer"
-  href={href}
-  target={external ? "_blank" : null}
+  href={href} target="_blank"
 >
   <slot />
 </a>
+
+{:else}
+
+<a
+  class="underline-offset-4 decoration-2 decoration-zinc-300 hover:decoration-primary-400 underline font-semibold cursor-pointer"
+  href={href} rel="prefetch"
+>
+  <slot />
+</a>
+
+{/if}
