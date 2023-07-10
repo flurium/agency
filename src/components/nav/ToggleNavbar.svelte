ceb6c9ea323936e2483e974e12be6ce1705d434d
Roman Koshchei <romankoshchei@gmail.com>
2023-07-10 16:39:35 +0300

    mobile navbar

--- src/components/nav/ToggleNavbar.svelte
+++ src/components/nav/ToggleNavbar.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
-  import type { NavbarTranslation } from '../../translations'
-  import Link from './Link.svelte'
+  import type { Language, NavbarTranslation } from '../../translations'
+  import { TwitterIcon, GitHubIcon } from "../icons"
+  import LanguageSwitch from './LanguageSwitch.svelte'
 
   export let t: NavbarTranslation
+  export let lang: Language
 
   let open: boolean = false
 </script>
 
-<nav class="block sm:hidden">
+<nav class="sm:hidden flex gap-4">
+  <LanguageSwitch {lang}/>
   <button class="p-3 rounded-md text-2xl hover:bg-secondary-100" aria-label="Navbar toggle button" on:click={() => open = !open}>
     <svg
       width="20"
@@ -25,18 +28,64 @@
 </nav>
 
 {#if open}
-  <nav class="absolute top-0 right-0 p-6 w-1/2 bg-white rounded-md flex flex-col gap-4 sm:hidden items-end">
-    <button class="p-3 rounded-md text-2xl hover:bg-secondary-100" on:click={() => open = !open}>
-      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M2 2L18 18M2 18L18 2" stroke="black" stroke-width="1.5"/>
-      </svg>
-    </button>
+  <div class="absolute top-0 right-0 py-6 w-full bg-white sm:hidden shadow-md z-20">
+    <div class="flex justify-between w-full items-center px-6">
+      <a class="select-none font-bold text-primary-500 text-3xl" href="/">FLURIUM</a>
 
-    <Link href={t.switch.route}>{t.switch.label}</Link>
-    <Link href="#our-work">{t.ourWork}</Link>
-    <Link href="#team">{t.team}</Link>
-    <a href="#contact" class="py-2 px-4 w-full rounded-md bg-primary-500 text-white text-center">
-      {t.contact}
+      <button class="p-3 rounded-md text-2xl hover:bg-secondary-100" on:click={() => open = !open}>
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M2 2L18 18M2 18L18 2" stroke="black" stroke-width="1.5"/>
+        </svg>
+      </button>
+    </div>
+
+
+    <nav class="flex flex-col my-6"> 
+      <a
+      class="hover:bg-secondary-100 py-2 px-6"
+      href={`/${lang}/#our-work`}
+      rel="prefetch"
+    >
+      {t.ourWork}
     </a>
-  </nav>
+    <a
+      class="hover:bg-secondary-100 py-2 px-6"
+      href={`/${lang}/#team`}
+      rel="prefetch"
+    >
+      {t.team}
+    </a>
+    <a
+      class="hover:bg-secondary-100 py-2 px-6"
+      href={`/blog/${lang}`}
+      rel="prefetch"
+    >
+      {t.blog}
+    </a>
+    </nav>
+    
+      <!-- <LanguageSwitch lang={lang} /> -->
+      
+    <div class="flex gap-4 items-center mx-6">
+      <span class=" leading-10">Social:</span> 
+      <a
+        class="hover:bg-secondary-100 w-12 h-12 flex justify-center items-center rounded-md"
+        href="https://twitter.com/fluriumteam"
+        target="_blank"
+      >
+        <TwitterIcon />
+      </a>
+      <a
+        class="hover:bg-secondary-100 w-12 h-12 flex justify-center items-center rounded-md"
+        href="https://github.com/flurium"
+        target="_blank"
+      >
+        <GitHubIcon />
+      </a>
+    </div>
+
+    <a href="#contact" class="py-2 px-4 block mx-6 mt-4 rounded-md bg-primary-500 text-white text-center">
+        {t.contact}
+    </a>
+  </div>
 {/if}
