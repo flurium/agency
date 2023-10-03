4438ce4c00dfaea904347798b52241260f201e67
Roman Koshchei <romankoshchei@gmail.com>
2023-10-03 13:55:53 +0300

    move to v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,29 @@
-import { defineConfig } from "astro/config";
-import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-import image from "@astrojs/image";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config"
+import svelte from "@astrojs/svelte"
+import tailwind from "@astrojs/tailwind"
+import vercel from "@astrojs/vercel/serverless"
+import sitemap from "@astrojs/sitemap"
 
-import prefetch from "@astrojs/prefetch";
+import prefetch from "@astrojs/prefetch"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), image(), sitemap({
-    i18n: {
-      defaultLocale: "uk",
-      locales: {
-        en: "en",
-        uk: "uk"
-      }
-    }
-  }), prefetch()],
+  integrations: [
+    svelte(),
+    tailwind(),
+    ,
+    sitemap({
+      i18n: {
+        defaultLocale: "uk",
+        locales: {
+          en: "en",
+          uk: "uk",
+        },
+      },
+    }),
+    prefetch(),
+  ],
   adapter: vercel(),
   output: "hybrid",
-  site: "https://flurium.com"
-});
+  site: "https://flurium.com",
+})
