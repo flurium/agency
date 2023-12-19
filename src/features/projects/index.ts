c360d848159b78f510e349aa4e2eb5412d60cc47
Roman Koshchei <romankoshchei@gmail.com>
2023-12-19 15:46:16 +0200

    portfolio

---
+++ src/features/projects/index.ts
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    name: "Spentoday",
+    description:
+      "Ukrainian e-commerce platform. Create an online shop fast and simple. No complexity, no hiring designers, no ton of services.",
+    slug: "spentoday",
+  },
+  {
+    name: "BidMe",
+    description: "Auction platform made with MVC in 2 weeks.",
+    slug: "bidme",
+  },
+  {
+    name: "Road to Vostok Remake",
+    description: "Performance improvements for Road to Vostok website.",
+    slug: "roadtovostok",
+  },
+  {
+    name: "Thing",
+    description: "MVC e-commerce website.",
+    slug: "thing",
+  },
+] as const
