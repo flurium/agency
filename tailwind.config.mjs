87e245eef1f6db419e3d493a391d1232a5f08eef
Roman Koshchei <romankoshchei@gmail.com>
2024-03-24 21:56:46 +0200

    poppins

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,14 @@
-const colors = require("tailwindcss/colors")
+import defaultTheme from "tailwindcss/defaultTheme"
+import colors from "tailwindcss/colors"
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         primary: colors.indigo,
         secondary: {
